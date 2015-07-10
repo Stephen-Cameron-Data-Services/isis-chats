b1b48a7c491a74efcf71ffd0f49d5d14d2c8d74a
stevecam62 <steve.cameron.62@gmail.com>
2015-07-10 17:39:06 +1000

    date testing

---
+++ dom/src/test/java/au/com/scds/chats/dom/modules/participant/DateTestTest.java
@@ -0,0 +1,36 @@
+package au.com.scds.chats.dom.modules.participant;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.junit.Assert.assertThat;
+
+import org.apache.isis.applib.value.Date;
+import org.junit.Before;
+import org.junit.Test;
+
+public class DateTestTest {
+	
+    DateTest date;
+
+    @Before
+    public void setUp() throws Exception {
+        date = new DateTest();
+    }
+
+    public static class DateTester extends DateTestTest {
+
+        @Test
+        public void happyCase() throws Exception {
+            // given
+            assertThat(date.getTest(), is(nullValue()));
+
+            // when
+            Date now = new Date();
+            Date later = now.add(1, 1, 1);
+            date.setTest(now);
+
+            // then
+            assertThat(date.getTest(), is(later));
+        }
+    }
+}
